c6e68b3786d26a001dd41bb2401cb34ab01a053c
Barry Mills <bpmills@outlook.com>
2016-06-13 22:39:09 +0100

    Created new MVC app and added Sponsors and About us

---
+++ DDDMidlands/DDDMidlands/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDDMidlands.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+
+        public ActionResult Sessions()
+        {
+            return View();
+        }
+
+        public ActionResult Sponsors()
+        {
+            return View();
+        }
+
+
+        public ActionResult Speakers()
+        {
+            return View();
+        }
+
+        public ActionResult Venue()
+        {
+            return View();
+        }
+    }
+}
---
+++ DDDMidlands/DDDMidlands/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DDDMidlands.MvcApplication" Language="C#" %>
